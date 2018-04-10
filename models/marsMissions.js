0569f3900dbc08c9237248ac74dcaf61d3cfba3a
Tyler Earls <tyler.a.earls@gmail.com>
2018-04-10 01:25:54 -0500

    added images, got them to display on show page

--- models/marsMissions.js
+++ models/marsMissions.js
@@ -4,35 +4,35 @@ const marsMissions = [
     launchDate: "26 Nov 2011",
     operator: "NASA",
     missionType: "Rover",
-    img: ""
+    img: "http://www.nasa.gov/sites/default/files/thumbnails/image/pia19808-main_tight_crop-monday.jpg"
   },
   {
     name: "Opportunity",
     launchDate: "8 Jul 2003",
     operator: "NASA",
     missionType: "Rover",
-    img: ""
+    img: "https://i.kinja-img.com/gawker-media/image/upload/s--_L41fIty--/c_scale,fl_progressive,q_80,w_800/kqgk38fm3xeuv8opppcx.jpg"
   },
   {
     name: "Spirit",
     launchDate: "10 Jun 2003",
     operator: "NASA",
     missionType: "Rover",
-    img: ""
+    img: "https://www.jpl.nasa.gov/missions/web/mer.jpg"
   },
   {
     name: "Sojourner",
     launchDate: "4 Dec 1996",
     operator: "NASA",
     missionType: "Rover",
-    img: ""
+    img: "https://img.purch.com/w/640/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzAwOS84NjAvaTAyL21hcnMtcGF0aGZpbmRlci1zb2pvdXJuZXIuanBnPzEzMDYzNTk5Nzg="
   },
   {
     name: "Rosetta",
     launchDate: "2 Mar 2004",
     operator: "ESA",
     missionType: "Gravity Assist",
-    img: ""
+    img: "https://3c1703fe8d.site.internapcdn.net/newman/csz/news/800/2006/rosettawarms.jpg"
   }
 ];
 
